c54592c06464d0ef8d8b7812f358105ee15de7d1
donramones <agusrivarolaa00@gmail.com>
2025-10-11 00:41:05 -0300

    10ok

---
+++ ejercicio_01/src/ejercicio_01/Titular.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_01;
+
+/**
+ *
+ * @author rivar
+ */
+public class Titular {
+    private String nombre;
+    private String dni;
+    private Pasaporte pasaporte;
+
+    public Titular(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public void setPasaporte(Pasaporte pasaporte) {
+        this.pasaporte = pasaporte;
+      
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public Pasaporte getPasaporte() {
+        return pasaporte;
+    }
+
+    @Override
+    public String toString() {
+        return "Titular{" + "nombre=" + nombre + ", dni=" + dni + ", pasaporte=" + pasaporte + '}';
+    }
+    
+    
+}
