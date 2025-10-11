af484b1ec5de212d9765008a16526a402cc87fd6
donramones <agusrivarolaa00@gmail.com>
2025-10-11 20:37:07 -0300

    10ok

--- ejercicio_03_/src/ejercicio_03_/Libro.java
+++ ejercicio_03_/src/ejercicio_03_/Libro.java
@@ -9,5 +9,40 @@ package ejercicio_03_;
  * @author rivar
  */
 public class Libro {
+    private String titulo;
+    private String isbn;
+    private Autor autor;
+    private Editorial editorial;
+
+    public Libro(String titulo, String isbn, Editorial editorial) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.editorial = editorial;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+    
+    
+
+ 
+    
     
 }
