1a0d7b6d74479b8a3ba6169f66d877f4bf612d4c
donramones <agusrivarolaa00@gmail.com>
2025-10-11 20:36:57 -0300

    10ok

--- ejercicio_03_/src/ejercicio_03_/Autor.java
+++ ejercicio_03_/src/ejercicio_03_/Autor.java
@@ -9,5 +9,24 @@ package ejercicio_03_;
  * @author rivar
  */
 public class Autor {
+    private String nombre;
+    private String nacionalidad;
+
+    public Autor(String nombre, String nacionalidad) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+            
+            
+            
+            
+
+    
     
 }
