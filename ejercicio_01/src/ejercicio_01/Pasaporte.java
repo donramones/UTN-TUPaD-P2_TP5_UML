481d1a2948660d8c5c80338af81b19af7d35bbc6
donramones <agusrivarolaa00@gmail.com>
2025-10-11 00:41:00 -0300

    10ok

---
+++ ejercicio_01/src/ejercicio_01/Pasaporte.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_01;
+
+/**
+ *
+ * @author rivar
+ */
+public class Pasaporte {
+    private String numero;
+    private String fechaEmision;
+    private Foto foto;
+    private Titular titular;
+
+    public Pasaporte(String numero, String fechaEmision, String imagen, String formato) {
+        this.numero = numero;
+        this.fechaEmision = fechaEmision;
+        this.foto = new Foto (imagen, formato);
+        this.titular = titular;
+        titular.setPasaporte(this);
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public Foto getFoto() {
+        return foto;
+    }
+
+    public Titular getTitular() {
+        return titular;
+    }
+   
+    
+    
+}
