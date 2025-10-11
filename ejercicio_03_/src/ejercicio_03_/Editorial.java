54f71c8f1bfe16aa6ae75aa487dee085be4cacaf
donramones <agusrivarolaa00@gmail.com>
2025-10-11 20:37:02 -0300

    10ok

--- ejercicio_03_/src/ejercicio_03_/Editorial.java
+++ ejercicio_03_/src/ejercicio_03_/Editorial.java
@@ -9,5 +9,18 @@ package ejercicio_03_;
  * @author rivar
  */
 public class Editorial {
+    private String nombre;
+    private String direccion;
+
+    public Editorial(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Editorial{" + "nombre=" + nombre + ", direccion=" + direccion + '}';
+    }
     
 }
+   
