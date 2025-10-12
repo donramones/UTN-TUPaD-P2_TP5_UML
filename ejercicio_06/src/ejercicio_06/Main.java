cc689c924581e87cef9ea0fe7fb896717d0bad54
donramones <agusrivarolaa00@gmail.com>
2025-10-12 11:56:40 -0300

    10ok

--- ejercicio_06/src/ejercicio_06/Main.java
+++ ejercicio_06/src/ejercicio_06/Main.java
@@ -14,7 +14,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cliente cliente1 = new Cliente ("Naruto Uzumaki","389-333-9876");
+        Cliente cliente2 = new Cliente ("Dan Kato","453-234-099");
+        
+        Mesa mesa1 = new Mesa ("02-B",4);
+        Mesa mesa2 = new Mesa ("01-A",2);
+        
+        System.out.println("\n--- Reservas ---");
+        System.out.println(cliente1.toString());
+        System.out.println(mesa1.toString());
+        
+        
     }
     
 }
