7817edf1aa05f7d985fbaf17a10b4610d5407837
donramones <agusrivarolaa00@gmail.com>
2025-10-12 11:56:48 -0300

    10ok

--- ejercicio_06/src/ejercicio_06/Reserva.java
+++ ejercicio_06/src/ejercicio_06/Reserva.java
@@ -9,5 +9,64 @@ package ejercicio_06;
  * @author rivar
  */
 public class Reserva {
+    private String fecha;
+    private String hora;
+    private Cliente cliente;
+    private Mesa mesa;
+
+    public Reserva(String fecha, String hora, Mesa mesa) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.mesa = mesa;
+    }
+
+    public Reserva(String fecha, String hora, Mesa mesa, Cliente cliente) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.mesa = mesa;
+        if (cliente !=null){
+            this.cliente = cliente;       
+        }
+        
+        
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "fecha=" + fecha + ", hora=" + hora + ", cliente=" + cliente + ", mesa=" + mesa + '}';
+    }
+    
     
 }
