7141d37ab6a3edd869bc16d66ce4656975c86e07
donramones <agusrivarolaa00@gmail.com>
2025-10-12 02:02:01 -0300

    10ok

---
+++ ejercicio_05/src/ejercicio_05/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_05;
+
+/**
+ *
+ * @author rivar
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Propietario prop1 = new Propietario("Akira", "29756899");
+        Computadora comp1 = new Computadora("Mac", "MacOS2025", "x99", "AmdMAc");
+        
+        System.out.println(prop1);
+        System.out.println(comp1);
+        
+        
+        
+        
+        
+    }
+    
+}
