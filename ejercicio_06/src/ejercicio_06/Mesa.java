96594dd8d9aefe88264eb23987b860f01d439c57
donramones <agusrivarolaa00@gmail.com>
2025-10-12 11:56:44 -0300

    10ok

--- ejercicio_06/src/ejercicio_06/Mesa.java
+++ ejercicio_06/src/ejercicio_06/Mesa.java
@@ -9,5 +9,38 @@ package ejercicio_06;
  * @author rivar
  */
 public class Mesa {
+    private String numero;
+    private int capacidad;
+
+    public Mesa(String numero, int capacidad) {
+        this.numero = numero;
+        this.capacidad = capacidad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesa{" + "numero=" + numero + ", capacidad=" + capacidad + '}';
+    }
     
-}
+    
+    
+
+}   
+
+  
