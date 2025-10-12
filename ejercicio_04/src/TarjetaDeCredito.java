4f019fd07c3705b6471c897071db05710fe85312
donramones <agusrivarolaa00@gmail.com>
2025-10-11 23:18:18 -0300

    10ok

---
+++ ejercicio_04/src/TarjetaDeCredito.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rivar
+ */
+public class TarjetaDeCredito {
+    private String numero;
+    private String fechaVencimiento;
+    private Banco banco;
+    private Cliente cliente;
+
+    public TarjetaDeCredito(String numero, String fechaVencimiento, Banco banco) {
+        this.numero = numero;
+        this.fechaVencimiento = fechaVencimiento;
+        this.banco = banco;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+        if(cliente !=null && cliente.getTarjeta() != this){
+            cliente.setTarjeta(this);
+        }
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarjeta:" + numero + ", vencimiento: " + fechaVencimiento ;
+    }
+    
+    
+    
+    
+}
