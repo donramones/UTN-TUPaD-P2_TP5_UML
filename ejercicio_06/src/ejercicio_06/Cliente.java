884d7cc3f6e309e73cae7ee21efd0f63f4407adf
donramones <agusrivarolaa00@gmail.com>
2025-10-12 11:56:34 -0300

    10ok

--- ejercicio_06/src/ejercicio_06/Cliente.java
+++ ejercicio_06/src/ejercicio_06/Cliente.java
@@ -9,5 +9,35 @@ package ejercicio_06;
  * @author rivar
  */
 public class Cliente {
+    private String nombre;
+    private String telefono;
+
+    public Cliente(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
+    
+    
     
 }
