981409180ab78aa9966c696c284b624e64c9d17f
donramones <agusrivarolaa00@gmail.com>
2025-10-11 23:18:22 -0300

    10ok

---
+++ ejercicio_04/src/Cliente.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rivar
+ */
+public class Cliente {
+    private String nombre;
+    private String dni;
+    private TarjetaDeCredito tarjeta;
+
+    public Cliente(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public void setTarjeta(TarjetaDeCredito tarjeta) {
+        this.tarjeta = tarjeta;
+        if(tarjeta !=null && tarjeta.getCliente() != this){
+            tarjeta.setCliente(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente:" + nombre + ", dni: " + dni;
+    }
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TarjetaDeCredito getTarjeta() {
+        return tarjeta;
+    }
+    
+            
+    
+}
