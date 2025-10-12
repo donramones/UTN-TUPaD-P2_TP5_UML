ba165fa0b6bbfa1fd8523b5cebae16790dea054b
donramones <agusrivarolaa00@gmail.com>
2025-10-11 23:18:28 -0300

    10ok

---
+++ ejercicio_04/src/Banco.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rivar
+ */
+public class Banco {
+    private String nombre;
+    private String cuit;
+
+    public Banco(String nombre, String cuit) {
+        this.nombre = nombre;
+        this.cuit = cuit;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco{" + "nombre=" + nombre + ", cuit=" + cuit + '}';
+    }
+    
+    
+    
+    
+}
