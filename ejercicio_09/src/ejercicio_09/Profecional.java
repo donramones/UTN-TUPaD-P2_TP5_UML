a15b69e396ef04d08a62aefaca313d0039ef5f16
donramones <agusrivarolaa00@gmail.com>
2025-10-17 21:24:30 -0300

    10ok

--- ejercicio_09/src/ejercicio_09/Profecional.java
+++ ejercicio_09/src/ejercicio_09/Profecional.java
@@ -9,5 +9,27 @@ package ejercicio_09;
  * @author rivar
  */
 public class Profecional {
+    private String nombreProfesional;
+    private String especialidad;
+
+    public Profecional(String nombreProfesional, String especialidad) {
+        this.nombreProfesional = nombreProfesional;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombreProfesional() {
+        return nombreProfesional;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Profecional{" + "nombreProfesional=" + nombreProfesional + ", especialidad=" + especialidad + '}';
+    }
+    
+    
     
 }
