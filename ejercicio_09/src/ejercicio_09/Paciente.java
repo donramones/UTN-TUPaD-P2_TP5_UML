11435b4e32a815362d44ad1ee233fd0057cb912c
donramones <agusrivarolaa00@gmail.com>
2025-10-17 21:22:38 -0300

    10ok

--- ejercicio_09/src/ejercicio_09/Paciente.java
+++ ejercicio_09/src/ejercicio_09/Paciente.java
@@ -9,5 +9,28 @@ package ejercicio_09;
  * @author rivar
  */
 public class Paciente {
+    private String nombre;
+    private String obraSocial;
+
+    public Paciente(String nombre, String obraSocial) {
+        this.nombre = nombre;
+        this.obraSocial = obraSocial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" + "nombre=" + nombre + ", obraSocial=" + obraSocial + '}';
+    }
+    
+    
+    
     
 }
