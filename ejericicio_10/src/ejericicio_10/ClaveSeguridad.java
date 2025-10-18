35b61fcf122a0bba73fd67ceb8f9a63dc7af7b8a
donramones <agusrivarolaa00@gmail.com>
2025-10-18 19:29:53 -0300

    10ok

---
+++ ejericicio_10/src/ejericicio_10/ClaveSeguridad.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejericicio_10;
+
+/**
+ *
+ * @author rivar
+ */
+public class ClaveSeguridad {
+    private String codigo;
+    private String ultimaModificacion;
+
+    public ClaveSeguridad(String codigo, String ultimaModificacion) {
+        this.codigo = codigo;
+        this.ultimaModificacion = ultimaModificacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getUltimaModificacion() {
+        return ultimaModificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "ClaveSeguridad{" + "codigo=" + codigo + ", ultimaModificacion=" + ultimaModificacion + '}';
+    }
+    
+    
+    
+}
