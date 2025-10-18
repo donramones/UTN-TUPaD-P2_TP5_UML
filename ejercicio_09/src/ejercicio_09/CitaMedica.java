c2594ab0fa90ebd8622f92f93117c74771ad4c50
donramones <agusrivarolaa00@gmail.com>
2025-10-17 21:27:32 -0300

    10ok

--- ejercicio_09/src/ejercicio_09/CitaMedica.java
+++ ejercicio_09/src/ejercicio_09/CitaMedica.java
@@ -9,5 +9,34 @@ package ejercicio_09;
  * @author rivar
  */
 public class CitaMedica {
+    private String fecha;
+    private String hora;
+    private Profecional profecional;
+    private Paciente paciente;
+
+    public CitaMedica(String fecha, String hora, Profecional profecional, Paciente paciente) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.profecional = profecional;
+        this.paciente = paciente;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public Profecional getProfecional() {
+        return profecional;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    
+    
     
 }
