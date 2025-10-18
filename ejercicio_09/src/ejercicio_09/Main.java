3a0747a1cf60bb47ea0d7f774ff6e3ac0b40c819
donramones <agusrivarolaa00@gmail.com>
2025-10-17 21:40:58 -0300

    10ok

--- ejercicio_09/src/ejercicio_09/Main.java
+++ ejercicio_09/src/ejercicio_09/Main.java
@@ -14,7 +14,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Paciente paciente1 = new Paciente("Sant Seiya", "Prometheus");
+        Profecional profecional1 = new Profecional("Dr Hause", "Clinica medica");
+        
+        System.out.println("Cita medica");
+        CitaMedica cita = new CitaMedica ("10-1-025", 10 30, paciente1, profecional1);
+        
+        
+        
+        
     }
     
 }
